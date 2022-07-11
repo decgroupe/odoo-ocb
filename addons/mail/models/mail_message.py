d5b4711f673b977930b31458d07a07f5b4478763
Yann Papouin <ypa@decgroupe.com>
2022-07-11 12:37:07 +0200

    [DEC][IMP] mail: Allow admin to see all mail messages

--- addons/mail/models/mail_message.py
+++ addons/mail/models/mail_message.py
@@ -355,7 +355,7 @@ class Message(models.Model):
                     model_record_ids.setdefault(vals['model'], set()).add(vals['res_id'])
             return model_record_ids
 
-        if self.env.is_superuser():
+        if self.env.is_superuser() or self.env.user.has_group("base.group_system"):
             return
         # Non employees see only messages with a subtype (aka, not internal logs)
         if not self.env['res.users'].has_group('base.group_user'):
